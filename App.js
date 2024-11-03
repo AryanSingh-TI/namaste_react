95391723c95b941891efa036305fc6c09a72096e
Aryan <aryan.prakhar@targetintegration.com>
2024-11-03 18:29:55 +0530

    episode 3 (part-1) =>  introduction to jsx and stuff about jsx

--- App.js
+++ App.js
@@ -1,43 +1,41 @@
-/** CREATING A SIMILAR HTML STRUCTURE USING REACT
- * 
- * <div id ="parent">
- * 
- *     <div id="child_1">   
- *          <h1></h1>
- *          <h2></h2>
- *     </div>
- * 
- *     <div id="child_2">
- *          <h3></h3>
- *          <h4></h4>
- *     </div>
- * 
- * </div>
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"},
-    //the third argument is used to pass the children of the tag while creating it
-    //when there are more than one children(siblings) we can pass an array instead
-    [
-        React.createElement("div", {id:"child_1"},
-            [
-                //similary we can add siblings here through a list
-                React.createElement("h1",{},"erm what the sigma"),
-                React.createElement("h2",{},"This is the H2 tag")
-            ]
-        ),
-        React.createElement("div", {id:"child_2"},
-            [
-                React.createElement("h3",{},"This is the H3 tag"),
-                React.createElement("h4",{},"This is the H4 tag")
-            ]
-        )
+//1) core React
+const heading = React.createElement(  
+    "h1", 
+    {id:"heading_1"}, 
+    "1) heading using the core createElement() from React" 
+);
 
-    ]
-)
+//2) jsx
+const heading_jsx = <h1 id="heading_2" className="heading" tabIndex="1">  2) Heading using jsx </h1>;  
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(parent)
-root.render(parent)
+//a) this is jsx, its an html like syntax in javascript but ITS NOT HTML INSIDE JAVASCRIPT (like literally)
+//b) both of these ways can be used to make React ELement(which is an object)
+//c) both of the heading_1 and heading_2 work the same way (we can create react elements using both ways)
+//d) just the syntax is different 
+//e) jsx is NOT a valid Javascript Code, when you paste "const heading_jsx = <h1 id="heading_2">2) Heading using jsx </h1>;"  in the console of a browser it will give you an error because javascript engine does not understand this piece of jsx code
+//f) the jsx code is TRANSPILED by PARCEL using it's BABEL package before it's rendered 
+//g) jsx (using BABEL package in PARCEL)=>(TRANSPILED into) => React.createElement (using React.createElement)=>(converts it to a) ReactElement(a JS object) (render())=>converts it to)  HTMLElement(to render on the DOM) 
+//h) another example of how jsx is not HTML is in it's attributes, class attribute of HTML tags is defined as className in jsx
+//i) every attribute of jsx is written in camel case
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+                          // both these console logs return ReactElements (objects) not HTML tags
+console.log(heading);     // objects as in key value pairs that contain various informations about the supposed HTML tags
+console.log(heading_jsx); // jsx also returns an object because jsx under the hood is converted into React.createElement as well
+                          
+
+const parent = React.createElement(
+    "div", 
+    {id:"parent"}, 
+    heading, 
+    heading_jsx //appending both the headings as children of a parent div
+);
+
+
+root.render(parent);//rendering the parent div
+                    //those React Objects are converted into html tags and rendered inside the root element
+                    //render() replaces the previous content of the Root element and does not append it
