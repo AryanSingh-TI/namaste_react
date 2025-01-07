3a06701213fd04034a9aa98ed254d891e110459a
Aryan <aryan.prakhar@targetintegration.com>
2025-01-07 19:09:14 +0530

    episode 8 (part-3) => class based component life cycle methods and about them

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,16 @@
 import {Component} from "react";
-//destructirng React on the fly this is like doing const {component} = React, this will get the Component child of the React Object so that we don't have to write React.Component when we inherit/extend React on our Class Based Component
 import User from "./User";
 import UserClass from "./UserClass";
-//this import statement will entail that the component has been mounted
-//now whenver the Class based component is being used inside the code
-//the class is instantiated and it's constructor is called
-//after the constructor is done setting up things like props and state variables
-//the render() method is then called to return the jsx=>converted to html that will render on our DOM
 
 class About extends Component
 {
     constructor(props)
     {
         super(props);
-        console.log("1 Parent constructor() is called [About is the parent of the UserClass Component because UserClass component is being called inside About]")
     }
 
     render()
     {
-        console.log("2 Parent render() is called")
         return (
             <div>
                 <h1>
@@ -36,9 +28,7 @@ class About extends Component
 
     componentDidMount()
     {
-        console.log("6 (Parent) About componentDidMount() is called")
-        //parent componentdidMount() is called after the child because the child is used inside the render of the parent
-        //and the parent will only finish it's rendering after the child has finished rendering and hence the componentdidMount() of the child will be called before the parent
+        console.log("Parent (About) componentDidMount() is called")
     }
 
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,57 +1,27 @@
-import React from "react";
-//This is how you create a class based component in React
-//To create a class based component in react you need to inherit React.Component from your custom class and React will start tracking that component
+import React, { forwardRef } from "react";
+
 class UserClass extends React.Component {
 
-    //constructor() method is used to recieve arguments given to the class, here it is recieved inside the props variable
     constructor(props)
     {
         super(props);
-        //super() method is used to set props for the parent class React.Component
-        //In order to properly utilize the passed arguments/props we need to set it for the parent class
-        //this.props will only be accessible after super(props) is called and the following props are set for the React.Component
 
-        //loading a class based component means creating an isntance of that class
-        //whenever an instance of the class is created it's constructor is called
-        //and this is when the state variables of this component are created and initialised
-        this.state = {
-            count:0,
-            count2:2
+        this.state = 
+        {
+            userInfo: 
+            {
+                name     : "Dummy",
+                location : "Default"
+            }
         }
-        console.log(props);
-        //first props will show Aryan in this component's constructor and then Elon Musk
-        console.log("3 Child(UserClass) constructor() is called")
-
     }
-    //render() method is used to return the jsx that we want to render from our component
 
-    render() {
+    render() 
+    {
+        const {name,location} = this.state.userInfo
 
-        console.log(this.props);
-        //first props will show Aryan in this component's render and then Elon Musk
-        console.log("4 Child(UserClass) render() is called");
-        const {name,location} = this.props
-        const {count,count2} = this.state
         return  ( 
             <div className="user-card">
-                <h2>count = {count}</h2>
-                <button 
-                    onClick={  
-                                ()=>{
-                                       this.setState({
-                                                        count:this.state.count+1,
-                                                        //count: here is used to specify the state variable whose value is being set, this.state.count is used to get the current value of the count and update it with the given operations
-                                                        count2:this.state.count2+2
-                                                    })
-                                       //this.setState can be used anywhere inside the class Component
-                                       //it is used to update the value of the state variable
-                                       //we cannot update it directly using this.state.count like that
-                                    }   
-                            }
-                >
-                    Count Increase
-                </button>
-                <h2>count2 = {count2}</h2>
                 <h2>Name : {name}</h2>
                 <h2>Location : {location}</h2>
                 <h2>twitter : doesntexist@404 </h2>
@@ -59,63 +29,112 @@ class UserClass extends React.Component {
         );
     }
 
-    componentDidMount()
+    async componentDidMount() // we can make the componentDidMount() Hook async in Class Based Components to make the API calls
     {
-        console.log("5 (Child) UserClass componentDidMount() function")
-        //this is called after the component has been successfully rendered 
-        //thus this is called after the render function
+        console.log("Child (UserClass) componentDidMount() is called")
+        //generally componentDidMount() is used to make API calls in Class Based Components after the compoment has been patched on to the webpage's actual DOM
 
-        //componentDidMount is used after all instances of that component have been constructed and rendered
-        //hence componentDidMount() is used(once) after the Aryan and the Elon Musk instances of the UserClass component is called 
-        //therefore number - 3(constructor) and number - 4(render) console logs of the Child Component UserClass will be display in the console log two times(one for Aryan instance and the other for Elon Musk Instance) first after the parent's (1-constructor and 2-render)
-        //and then the componentDidMount - 5 will be called(once) after all isntances of the UserClass component have been constructed and rendered
+        const data = await fetch("https://api.github.com/users/akshaymarch7");
+        const json = await data.json();
+
+        this.setState
+        ({
+            userInfo:json,
+        })
+
+    }
+
+    componentDidUpdate()
+    {
+        console.log("Child (UserClass) componentDidUpdate() is called");
+    }
+
+    componentWillUnmount()
+    {
+        console.log("compoentWillUnmount() is called when the component isn't showing up on the page");
+        //it happens when we move to another page such that the compoent isn't visible or used anymore on the DOM page then it is called
     }
 }
 
 export default UserClass;
 
-//"render phase"  [      MOUNTING             UPDATING               UNMOUNTING
-//  on the code   |         |                     |                      |       
-//  end, doesn't  |     constructor               |                      |
-//  affect the    |         |                     |                      |
-//  actual DOM    [         |________render_______|                      | 
-//                          |                     |                      |
-//"commit phase"  [   REACT actually updates the DOM and refs            |
-// reflect things |         |                     |                      |
-// on the DOM     [   componentDidMount    componentDidUpdate    componentDidUnmount
-//
-// example:- New props, setState(), forceUpdate() are used in the UPDATING process which updates the vritual DOM and then chose changes are propagated to the actual DOM
+// MOUNTING CYCLE (mounting cycle finishes after the component is constructed and rendered once with usually some dummy data)
+//  [ first the component is constructed and the state variable is assigned the default value
+//  | then the component is rendered with the default value (incorporated in the virtual DOM)
+//  | After render happens then REACT will update the actual DOM with the dummy data
+//  | then componentDidMount() will be called to signal that the component is mounted with the dummy data
+//  [ and in this componentDidMount() an API call was made that changed the state variable(by calling setState()) triggering the UPDATE cycle
+
+// UPDATE CYCLE 
+//  [ after the state variable is updated(by calling setState()) react rerenders the Component with the Updated Value
+//  | then again after the render in the vritual DOM, react updates and reflects the changes in the Actual DOM
+//  [ then componentDidUpdate() is called again to signal the successfully incorporating of the changed component in the actual DOM
 
 
-// - Parent Constructor                                            ] "render phase"
-// - Parent render                                                 | this is the phase in which
-//                                                                 | REACT constructs and renders
-//    -  First Child Constructor (Aryan Constructor)               | the component in the Virtual
-//    -  First Child render (Aryan render)                         | DOM, it is done here because
-//                                                                 | updating the actual DOM
-//    -  Second Child Constructor (Elon Constructor)               | dynamically and repeatedly
-//    -  Second Child render (Elon render)                         | could be a really expnesive
-//                                                                 | process in REACT.
-//    ...(etc)                                                     ]       
-//     
-//     - First Child ComponentDidMount (Aryan)                     ] "commit phase"
-//     - Second Child ComponentDidMount (Elon)                     | this is the phase in which 
-//     ...(etc)                                                    | react updates the actual
-//                                                                 | DOM and reflects the changes
-// - Parent ComponentDidMount                                      ] on our webpage.
-//
-// Since, the render phase is fast, REACT batches the render phase.
-// That is why the constructor and render of all the Child Instances of the Component are called before any of their componentDidMount is called.
-// because the constructor and render operations are a part of the render phase and it happens all at once for all the instances of a component.
-// And since, the commit phase is slow and costly it is called in the end for all instances of the Component at once
-// and commit phase updates the actual DOM and renders the changes on our webpage.
-//
-// Therefore, the render phase batches all the instances of a component 
-// and hence constructor() and render() of all the instances of a component are called 
-// before the commit phase and updating the actual DOM
-// The render phase updates the vritual DOM
-//
-// commit phase include actually incorporating the Component and it's instances on the webpage's actual DOM
-// and then call componentDidMount() Hook to signal that the component has been mounted on the DOM.
+/**
+ * MOUNTING 
+ * - constructor(state = dummy)
+ * - render(with dummy data)
+ *      <HTML (Dummy)/>
+ * - componentDidMount()
+ *      <API Call/>
+ *      <this.setState> -> State variable is updated
+ * 
+ * 
+ * UPDATING
+ * - re-render(API data)
+ *      <HTML (new API data)/>
+ * - componentDidUpdate()
+ * 
+ * 
+ * UNMOUNTING
+ * - componentWillUnmount() is called when we move to a different page such that the component is no longer visible
+ */
 
-// THIS IS A PART OF REACT'S OPTIMIZATION
+
+/**
+ * componentDidMount (Class Based Component Life cycle methods) !=  useEffect() (Functional Based Component Hook)
+ * 
+ * because :-
+ * 1) useEffect() is called after every render
+ * 2) componentDidMount() is called after the first render and when the component is updated every time, componentDidUpdate() is called
+ * 
+ */
+
+/**
+ * componentWillUnmount() life cycle method is used to clear stuff that was added by the component after it stopped rendering
+ * 
+ * example:-
+ * let's say we add a setInterval() inside our componentDidMount() to console log something after every second
+ * the set interval will not go away when we move to a different page
+ * it is the drawback of a single page application that is provided by React
+ * It happens because React doesn't reload the page just rerenders that particular component on the page
+ * so the interval added by the component stays in the browser's storage and everytime we go view that component a new interval is added on top of it
+ * this is dangerous because it can may crash an application as with multiple clicks multipe setInterval() is added
+ * 
+ * componentWillUnmount()
+ * {
+ *      clearInterval(this.that_interval);
+ * }
+ * therefore in componentWillUnmount() we use clearInterval(this.that_interval) to clear the interval everytime we move to a different page where the component is no longer rendered
+ * 
+ * 
+ * 
+ * The equivalent of this in Functional Based Component is provided by useEffect() Hook
+ * 
+ * example:- in useEffect if we similarly do a setInterval() to console log something after every second
+ * the setInterval will not go away when we move to a different page
+ * however this can be achieved by using return inside the useEffect() to clear the interval
+ * 
+ * useEffect()
+ * {
+ *      const timer = setInterval(()=>
+ *          console.log("print something");
+ *      ),1000);
+ *      
+ *      return() => {
+ *          clearInterval(timer);
+ *      }
+ * } 
+ * 
+ */
